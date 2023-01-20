0a1ea1cfbbe83d4e3fecc45724dc192c15197eaf
Evgenij Pashkin <e.n.pashkin@gmail.com>
2023-01-20 13:33:02 +0300

    Test1 commit First branch

---
+++ src/test1/Test1.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        task1();
+    }
+    private static void task1() {
+        System.out.println("First branch");
+    }
+}
